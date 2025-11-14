4c64255b08a2e2668f55d0003312939c13af1284
pycheol07-create <pycheol07@gmail.com>
2025-11-14 15:40:14 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App.js';
+
+// --- From index.tsx ---
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error("Could not find root element to mount to");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<React.StrictMode><App /></React.StrictMode>);
